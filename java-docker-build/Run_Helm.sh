bce6751537d0dada8edf1740a693b3713395cfd9
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-01-31 14:53:33 +0400

    Update Run_Helm.sh

--- java-docker-build/Run_Helm.sh
+++ java-docker-build/Run_Helm.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
+imageName="myanees/aneesm"
 timestamp=$(date +%s)
-imagever=myanees/aneesm:v$timestamp
+tagVal=v$timestamp
+imagever=$imageName:v$timestamp
 APIKEY=${clogin}
 KUBECONFIG=${kubeconfig}
 DLOGIN=${dlogin}
 DPASS=${dpass}
+
 docker login --username $DLOGIN --password $DPASS
 docker build -t $imagever .
 docker push $imagever
 ibmcloud login --apikey $APIKEY -r eu-de
 ibmcloud ks cluster-config mycluster
 export $KUBECONFIG
-#export DEPNAME=$(kubectl get deployment -o json | jq '.items[0].metadata.name')
+##export DEPNAME=$(kubectl get deployment -o json | jq '.items[0].metadata.name')
 export DEPNAME=$(kubectl get deployment --selector=$sel --output=jsonpath={.items..metadata.name})
 echo $DEPNAME
 if [[ $DEPNAME == *"helmsample"* ]]
 then
   echo "Updating container IMAGE"
-  helm upgrade love local/test --set=$imagever
+  helm upgrade helmsample local/test --set=$imagever
+  helm upgrade helmsample test1 --set=$imageName.tag=$tagVal
 else
   echo "Creating HELM PACKAGE"
-helm package test1
-helm install test1-0.1.2.tgz --name=helmsample
-#  kubectl create -f exposeservice.yml
-fi
+  helm package test1
+  helm install test1-0.1.2.tgz --name=helmsample
+  kubectl create -f exposeservice.yml
+#fi
