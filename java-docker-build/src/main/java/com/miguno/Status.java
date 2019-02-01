4928c0c6a18b3e8119364afe82c0008ec811f584
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-02-01 18:15:17 +0400

    Update Status.java

--- java-docker-build/src/main/java/com/miguno/Status.java
+++ java-docker-build/src/main/java/com/miguno/Status.java
@@ -11,6 +11,6 @@ public class Status {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public String statusInformation() {
-    return "{\"status\": \"HELM PACKAGE008\"}\n";
+    return "{\"status\": \"HELM PACKAGE009\"}\n";
   }
 }
